cc3aee82e65f75acea7592e11a5bb64b6f2a9888
Raoufroufa <zerdoudi.abderrraouf@gmail.com>
2023-04-30 01:21:55 +0100

    change port

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,7 @@ mongoose.connection.once('open', function(){
 
 
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(5000, () => console.log('Server started on port 5000'));
 
 
 // initiallize routes 
